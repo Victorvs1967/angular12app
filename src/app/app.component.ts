e4eb29e441242af1248a3a1785942e17c4d2bcca
Victors1967 <victorsmirnov@me.com>
2021-04-27 16:53:50 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './service/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(public data: DataService) {}
+
+  ngOnInit(): void {
+  }
   title = 'angular12app';
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  API_KEY = '4Jq3R9OjoQeZv8M5epZVg8lNDM';
+  API_KEY = '75c2bb76933b4995ba27d29c4404c1bf';
 
   constructor(private httpClient: HttpClient) { }
 
-  getNews() {
-
+  getNews(): Observable<any> {
+    return this.httpClient.get(`https://newsapi.org/v2/everything?q=DevOps&sortBy=popularity&apiKey=${this.API_KEY}`);
   }
 }
